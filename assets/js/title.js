4169c357754dd9d5f259d3a543e95e0715166a67
jianjun <daydayupls2021@gmail.com>
2023-09-23 15:36:49 +0800

    Update title.js

--- assets/js/title.js
+++ assets/js/title.js
@@ -9,6 +9,7 @@
 
 function set_title(){
     const app_url = window.location.href;
+    console.log(app_url);
     if(app_url.includes('index.html')){
         document.title = 'Home - DayDayUp Language School';
     }else if(app_url.includes('teachers.html')){
