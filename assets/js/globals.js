c1addcf50347182fba4bdded6f67b1cc4dda2184
jianjun <daydayupls2021@gmail.com>
2023-10-06 22:39:36 +0800

    update

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -11,7 +11,7 @@ function set_page() {
     const app_url = window.location.href;
     const app_name = 'DayDayUp Language School';
     var outlet_arr = [
-        ['Khatib', '846 YiShun Ring Road #01-3631（level 2）<br/>Singapore 760846 <br/><br/><strong>Phone:</strong> 6530 3063<br/><strong>Email:</strong> daydayupls21@gmail.com']
+        ['Khatib', '846 YiShun Ring Road #01-3631（level 2）<br/>Singapore 760846 <br/><br/><strong>Phone:</strong> 6530 3063<br/><strong>Email:</strong> daydayupls21@gmail.com','assets/attachment/daydayup1.jpg']
     ];
 
 
@@ -64,7 +64,7 @@ function set_page() {
     if(app_url.includes('contact.html')){
         var html = '';
         outlet_arr.forEach((outlet, key) => {
-            html += '<div class="footer-top"><div class="container"><div class="row"><div class="col-lg-6 footer-contact"><h3 class="app_name_'+key+'">'+app_name+' ('+outlet[0]+')'+'</h3></div><div class="col-lg-6 footer-contact"><p class="app_address_'+key+'">'+outlet[1]+'</p></div></div></div></div>';
+            html += '<div class="footer-top"><div class="container"><div class="row"><div class="col-lg-4 footer-contact"><h3 class="app_name_'+key+'">'+app_name+' ('+outlet[0]+')'+'</h3></div><div class="col-lg-4 footer-contact"><p class="app_address_'+key+'">'+outlet[1]+'</p></div><div class="col-lg-4 footer-contact"><img src="'+outlet[2]+'"   class="img-fluid app_img_'+key+'"></div></div></div></div>';
         });
         $('#footer').html(html);
     }else{
