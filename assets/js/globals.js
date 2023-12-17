173442ee6acfd648fd800bafae9caa858734ba61
jianjun <40233106+jianjunruan@users.noreply.github.com>
2023-12-17 14:56:35 +0800

    copyright

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -60,17 +60,23 @@ function set_page() {
     $('.app_contact').text('Contact');
     $('.get-started-btn').text('Get Started');
 
+
+    var copyright = 'All images on this website are copyrighted by their respective owners. We respect the copyrights of others and do not knowingly infringe on them. If you believe that any of the images on this website infringes on your copyright, please contact us and we will remove it immediately.';
+
     
     if(app_url.includes('contact.html')){
         var html = '';
         outlet_arr.forEach((outlet, key) => {
             html += '<div class="footer-top"><div class="container"><div class="row"><div class="col-lg-4 footer-contact"><h3 class="app_name_'+key+'">'+app_name+' ('+outlet[0]+')'+'</h3></div><div class="col-lg-4 footer-contact"><p class="app_address_'+key+'">'+outlet[1]+'</p></div><div class="col-lg-4 footer-contact"><img src="'+outlet[2]+'"   class="img-fluid app_img_'+key+'"></div></div></div></div>';
         });
+        html += '<div class="footer-top"><div class="container"><div class="copyright-msg">'+copyright+'</div></div></div>';
         $('#footer').html(html);
     }else{
         $('.app_address').html("846 YiShun Ring Road #01-3631（level 2）<br/>Singapore 760846 <br/><br/><strong>Phone:</strong> 6530 3063<br/><strong>Email:</strong> daydayupls21@gmail.com");
     }
     
+
+    $('.copyright-msg').html(copyright);
 
     
 }
